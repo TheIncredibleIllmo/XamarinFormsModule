3fad662763bc72425081b0e809c25537757aac05
Eduardo Medina <eduardomedm@gmail.com>
2018-12-08 11:59:26 -0600

    -Menu Page

---
+++ XamarinFormsModule/Views/MenuPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsModule.Views
+{
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
